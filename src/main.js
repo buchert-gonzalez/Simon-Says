91a4c517ac4ab1d2185b7894cc925a374dea8bb0
Santino-Gonzalez <96146946+Santino-Gonzalez@users.noreply.github.com>
2022-01-24 13:38:58 -0300

    CSS Transición
    Se agrega CSS para la transición en la opacidad para resaltar los colores y mostrar la secuencia a copiar.
    
    Co-Authored-By: Facundo-Buchert <96147040+Facundo-Buchert@users.noreply.github.com>
    Co-Authored-By: Santino-Gonzalez <96146946+Santino-Gonzalez@users.noreply.github.com>

--- src/main.js
+++ src/main.js
@@ -1,13 +1,38 @@
 let round = 0
 let userSequence = []
 let machineSequence = []
+
+const sound1 = new Audio("sounds/1.mp3")
+const sound2 = new Audio("sounds/2.mp3")
+const sound3 = new Audio("sounds/3.mp3")
+const sound4 = new Audio("sounds/4.mp3")
+
 function updateStatus(status, typeAlert){
     const $statusText = document.querySelector("#status")
         $statusText.innerText = status
         $statusText.className = typeAlert
 }
+
+function reset(){
+    round = 0
+    userSequence = []
+    machineSequence = []
+}
+
 function highlight($color){
     $color.style.opacity = 1
+    if($color.id === "green"){
+        sound1.play()
+    }
+    if($color.id === "red"){
+        sound2.play()
+    }
+    if($color.id === "yellow"){
+        sound3.play()
+    }
+    if($color.id === "blue"){
+        sound4.play()
+    }
     setTimeout(function(){
         $color.style.opacity = 0.5
     }, 250)
@@ -16,6 +41,10 @@ function highlight($color){
 function blockUserClick(){
     document.querySelectorAll(".color").forEach(function($colors){
         $colors.onclick = function(){
+            document.querySelector("#status").classList.add("trembles")
+            setTimeout(function(){
+                document.querySelector("#status").classList.remove("trembles")
+            }, 175);
         }
     })
 }
@@ -49,6 +78,83 @@ function showMachineSequence(){
     userSequence = []
     round++
     document.querySelector("#rounds").innerText = `Ronda Nº ${round}`
+    if(round === 10){
+        const $alertCongratulations = document.createElement("div")
+        $alertCongratulations.className = "alert alert-warning alert-dismissible fade show"
+        $alertCongratulations.setAttribute("role", "alert")
+        const $textCongratulations = document.createElement("strong")
+        $textCongratulations.innerText = "Felicidades! Has llegado a la ronda N°10!"
+        const $buttonClose = document.createElement("button")
+        $buttonClose.type = "button"
+        $buttonClose.className = "btn-close"
+        $buttonClose.setAttribute("data-bs-dismiss", "alert")
+        $buttonClose.setAttribute("aria-label", "Close")
+        $alertCongratulations.appendChild($textCongratulations)
+        $alertCongratulations.appendChild($buttonClose)
+        const $containerInterface = document.querySelector("#container-interface")
+        $containerInterface.appendChild($alertCongratulations)
+        $buttonClose.onclick = function(){
+            $containerInterface.removeChild($alertCongratulations)
+        }
+        setTimeout(function(){
+            if($alertCongratulations.parentNode === $containerInterface){
+                $containerInterface.removeChild($alertCongratulations)
+            }
+        }, 5000);
+    }
+
+    if(round === 20){
+        const $alertCongratulations = document.createElement("div")
+        $alertCongratulations.className = "alert alert-warning alert-dismissible fade show"
+        $alertCongratulations.setAttribute("role", "alert")
+        const $textCongratulations = document.createElement("strong")
+        $textCongratulations.innerText = "Felicidades! Has llegado a la ronda N°20, pocos llegan hasta aqui."
+        const $buttonClose = document.createElement("button")
+        $buttonClose.type = "button"
+        $buttonClose.className = "btn-close"
+        $buttonClose.setAttribute("data-bs-dismiss", "alert")
+        $buttonClose.setAttribute("aria-label", "Close")
+        $alertCongratulations.appendChild($textCongratulations)
+        $alertCongratulations.appendChild($buttonClose)
+        const $containerInterface = document.querySelector("#container-interface")
+        $containerInterface.appendChild($alertCongratulations)
+        $buttonClose.onclick = function(){
+            $containerInterface.removeChild($alertCongratulations)
+        }
+    }
+
+    if(round === 30){
+        const $alertCongratulations = document.createElement("div")
+        $alertCongratulations.className = "alert alert-warning alert-dismissible fade show"
+        $alertCongratulations.setAttribute("role", "alert")
+        const $textCongratulations = document.createElement("strong")
+        $textCongratulations.innerText = "Felicidades! Has llegado a la ronda N°30! Nos vemos en la N°50😁"
+        const $buttonClose = document.createElement("button")
+        $buttonClose.type = "button"
+        $buttonClose.className = "btn-close"
+        $buttonClose.setAttribute("data-bs-dismiss", "alert")
+        $buttonClose.setAttribute("aria-label", "Close")
+        $alertCongratulations.appendChild($textCongratulations)
+        $alertCongratulations.appendChild($buttonClose)
+        const $containerInterface = document.querySelector("#container-interface")
+        $containerInterface.appendChild($alertCongratulations)
+        $buttonClose.onclick = function(){
+            $containerInterface.removeChild($alertCongratulations)
+        }
+    }
+
+}
+
+function lose(){
+    updateStatus(`Has perdido! Toca "Empezar" para jugar devuelta.`, "alert alert-danger")
+    document.querySelector("#status").classList.add("trembles")
+    setTimeout(function(){
+        document.querySelector("#status").classList.remove("trembles")
+    }, 175);
+    blockUserClick()
+    reset()
+}
+
 function handleUserClicks(e){
     const $color = e.target
     highlight($color)
@@ -75,6 +181,7 @@ function handleRound(){
 }
 
 function startGame(){
+    reset()
     handleRound()
 }
 
